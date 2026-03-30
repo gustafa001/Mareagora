18923771ceb25bf4a2dbe16fba01c83a53108772
gustafa001 <89626751+gustafa001@users.noreply.github.com>
2026-03-29 22:04:54 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2920008879492175"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </head>
       <body className="font-dm-sans">

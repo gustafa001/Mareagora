b0a16aff5bdb2fd6bc8714095eeb94f741b1636b
gustafa001 <89626751+gustafa001@users.noreply.github.com>
2026-03-31 03:47:51 -0300

    Update tideUtils.ts

--- lib/tideUtils.ts
+++ lib/tideUtils.ts
@@ -285,3 +285,11 @@ export function getTideStatus(
 
   return { rising, next };
 }
+
+/**
+ * Converte graus em direção cardinal
+ */
+export function degToCompass(deg: number): string {
+  const dirs = ['N','NNE','NE','ENE','E','ESE','SE','SSE','S','SSO','SO','OSO','O','ONO','NO','NNO'];
+  return dirs[Math.round(deg / 22.5) % 16];
+}

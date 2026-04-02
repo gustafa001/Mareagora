07941e79830854bf28e230462720f6fe9423ac4a
gustafa001 <89626751+gustafa001@users.noreply.github.com>
2026-04-02 16:42:26 -0300

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import MobileStickyAd from "@/components/ads/MobileStickyAd";
+import { ReactNode } from 'react';
+import MobileStickyAd from '@/components/ads/MobileStickyAd';
 
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Providers({ children }: { children: ReactNode }) {
   return (
     <>
       {children}

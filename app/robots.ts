c4a0d6691e3e3241106e2e7c682b061798ed743a
gustafa001 <89626751+gustafa001@users.noreply.github.com>
2026-07-29 05:32:36 -0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -8,13 +8,13 @@ export default function robots(): MetadataRoute.Robots {
       disallow: ['/api/', '/admin/'],
     },
     sitemap: [
-      'https://mareagora.com.br/sitemap/index.xml',
-      'https://mareagora.com.br/sitemap/praias.xml',
-      'https://mareagora.com.br/sitemap/portos.xml',
-      'https://mareagora.com.br/sitemap/estados.xml',
-      'https://mareagora.com.br/sitemap/blog.xml',
-      'https://mareagora.com.br/sitemap/mundo.xml',
-      'https://mareagora.com.br/sitemap/tide-en.xml',
+      'https://www.mareagora.com.br/sitemap/index.xml',
+      'https://www.mareagora.com.br/sitemap/praias.xml',
+      'https://www.mareagora.com.br/sitemap/portos.xml',
+      'https://www.mareagora.com.br/sitemap/estados.xml',
+      'https://www.mareagora.com.br/sitemap/blog.xml',
+      'https://www.mareagora.com.br/sitemap/mundo.xml',
+      'https://www.mareagora.com.br/sitemap/tide-en.xml',
     ],
   };
 }

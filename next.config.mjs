21deb10fcaa5653fb82cfcfd370d45d5e497b060
gustafa001 <89626751+gustafa001@users.noreply.github.com>
2026-08-12 18:49:13 -0300

    feat: adiciona X-Content-Type-Options e Permissions-Policy

--- next.config.mjs
+++ next.config.mjs
@@ -33,8 +33,10 @@ const nextConfig = {
           { key: 'X-Frame-Options', value: 'DENY' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-          { key: 'Content-Security-Policy', value: "frame-ancestors 'none';" }
+          { key: 'Content-Security-Policy', value: "frame-ancestors 'none';" },
+          { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' }
         ]
       },
       {
@@ -42,6 +44,7 @@ const nextConfig = {
         headers: [
           { key: 'X-XSS-Protection', value: '1; mode=block' },
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           { key: 'Content-Security-Policy', value: "frame-ancestors *;" }
         ]
